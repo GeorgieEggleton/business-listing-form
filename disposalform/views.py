a7efcd3d5f963412573f9d343fb8e434912ee655
GeorgieEggleton <georgie@sailingfaster.co.uk>
2023-09-24 10:16:27 +0000

    debug error with saving vendor info to database

--- disposalform/views.py
+++ disposalform/views.py
@@ -12,17 +12,14 @@ class BusinessList(generic.ListView):
     template_name = 'index.html'
 
 
-class VendorInput(View):
 
+class VendorInput(View):
     def get(self, request, *args, **kwargs):
-        
         if request.user.is_authenticated: # check to see if the user is logged in
             try:  # This will check if the "Vendor.objects.get" command does not throw an error
                 vendor = Vendor.objects.get(username = request.user.get_username())  # this will look for the variable username in the "Vendor" table 
             except Vendor.DoesNotExist: # if the record does not exist in the "Vendor" database return text below 
                 vendor = "Vendor Does Not Exist" #text assigned to variable vendor if the record "username" doesn not exist 
-
-        
 
         return render(
             request,
@@ -34,7 +31,6 @@ class VendorInput(View):
     )
 
     def post(self, request, *args, **kwargs):
-        
         if request.user.is_authenticated: # check to see if the user is logged in
             try:  # This will check if the "Vendor.objects.get" command does not throw an error
                 vendor = Vendor.objects.get(username = request.user.get_username())  # this will look for the variable username in the "Vendor" table 
@@ -44,14 +40,12 @@ class VendorInput(View):
         #Take the information typed by the user in the form and create an instance of the class "VendorForm" from the forms.py file.  
         #This will be called "vendorform" 
         vendor_form = VendorForm(data=request.POST)  
-        
         if vendor_form.is_valid(): #if crispy forms thinks that the user input is "valid" i.e. if the date is in in the wrong format, or all the fields are not complete
             Error = "we are in the if statement!!!"
-            #vendor_form.instance.username = request.user.username
-            #vendor_form.instance.email = request.user.email
-            #vendor = vendor_form.save(commit=False)
-            #vendor.post = post
-            #vendor.save()
+            vendor_form.instance.username = request.user.username
+            vendor_form.instance.email = request.user.email
+            vendor = vendor_form.save(commit=False)
+            vendor.save()
         else:
             #vender_form = VendorForm() # if form invalid return empty form    
             Error = "we are not in the if statement"
@@ -67,11 +61,8 @@ class VendorInput(View):
 
 
 class BusinessInput(View):
-
     def get(self, request, *args, **kwargs):
-        
        #if request.user.is_authenticated: # check to see if the user is logged in
-
         return render(
             request,
             "business_input.html",
